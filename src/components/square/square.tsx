5b53db75133f6862a130191c166e2c6c91d6a5a7
Mikhail Slutskii <mikhail.slutskii@brotherhood.software>
2026-04-23 15:38:23 +0500

    Change text on "New text 2"

--- src/components/square/square.tsx
+++ src/components/square/square.tsx
@@ -1,7 +1,7 @@
 import styles from "./square.module.scss";
 
 const Square = () => {
-  return <div className={styles["square"]}>new text 1</div>;
+  return <div className={styles["square"]}>new text 2</div>;
 };
 
 export default Square;
