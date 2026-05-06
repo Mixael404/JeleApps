f5910cde82a9ecc92f3213315aa7eb68c95e189a
Mikhail Slutskii <mikhail.slutskii@brotherhood.software>
2026-05-06 16:50:00 +0500

    Minor changes in square component

--- src/components/square/square.tsx
+++ src/components/square/square.tsx
@@ -1,7 +1,7 @@
 import styles from "./square.module.scss";
 
 const Square = () => {
-  return <div className={styles["square"]}>new text 33</div>;
+  return <div className={styles["square"]}>new text 444</div>;
 };
 
 export default Square;
